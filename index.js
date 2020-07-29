936b92ddd42a8908b1ba00ccee05f1ae4ae40ac9
oshoferyuk <oshoferyuk@gmail.com>
2020-07-29 07:58:06 +0300

    initial

---
+++ index.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+require('dotenv').config()
+
+const host = process.env.HOST;
+const port = process.env.PORT;
+
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(`{"message": "Hello CI!"}`);
+});
+
+
+server.listen(port, host, () => {
+    console.log(`Server running at http://${host}:${port}/`)
+})
